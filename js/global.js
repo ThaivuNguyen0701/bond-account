7127ed2db2ab80af88b9df26b3e7d6c346dbbc55
ThaivuNguyen0701 <n.thaivu1512@gmail.com>
2022-06-24 13:34:16 +0700

    last commit

--- js/global.js
+++ js/global.js
@@ -35,66 +35,65 @@
     });
     
     // Cusom select box to style
-function customSelectBox() {
-    // Iterate over each select element
-    $('select').each(function () {
-        // Cache the number of options
-        var $this = $(this),
-            numberOfOptions = $(this).children('option').length;
+    function customSelectBox() {
+        // Iterate over each select element
+        $('select').each(function () {
+            // Cache the number of options
+            var $this = $(this),
+                numberOfOptions = $(this).children('option').length;
 
-        $this.addClass('select-hidden'); // Hides the select element
-        $this.wrap('<div class="custom-select-wrap"></div>'); // Wrap the select element in a div
-        $this.after('<div class="custom-select"></div>'); // Insert a styled div to sit over the top of the hidden select element
-        let $custom_select = $this.next('div.custom-select'); // Cache the styled div
-        $custom_select.text($this.children('option').eq(0).text()); // Show the first select option in the styled div
+            $this.addClass('select-hidden'); // Hides the select element
+            $this.wrap('<div class="custom-select-wrap"></div>'); // Wrap the select element in a div
+            $this.after('<div class="custom-select"></div>'); // Insert a styled div to sit over the top of the hidden select element
+            let $custom_select = $this.next('div.custom-select'); // Cache the styled div
+            $custom_select.text($this.children('option').eq(0).text()); // Show the first select option in the styled div
 
-        // Insert an unordered list after the styled div and also cache the list
-        let $list = $('<ul />', { 'class': 'options' }).insertAfter($custom_select);
-        // Insert a list item into the unordered list for each select option
-        for (let i = 0; i < numberOfOptions; i++) {
-            let $rel_val = $this.children('option').eq(i).val();
-            let $opts_cl = (i == 0) ? "selected" : "";
-            $('<li />', {
-                text: $this.children('option').eq(i).text(),
-                rel: $rel_val,
-                class: $opts_cl
-            }).appendTo($list);
-        }
-        // Cache the list items
-        let $listItems = $list.children('li');
-        // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
-        $custom_select.click(function (e) {
-            e.stopPropagation();
-            if(!$(this).hasClass('active')){
-                $('div.custom-select.active').each(function () {
-                    $(this).removeClass('active').next('ul.options').hide();
-                });
+            // Insert an unordered list after the styled div and also cache the list
+            let $list = $('<ul />', { 'class': 'options' }).insertAfter($custom_select);
+            // Insert a list item into the unordered list for each select option
+            for (let i = 0; i < numberOfOptions; i++) {
+                let $rel_val = $this.children('option').eq(i).val();
+                let $opts_cl = (i == 0) ? "selected" : "";
+                $('<li />', {
+                    text: $this.children('option').eq(i).text(),
+                    rel: $rel_val,
+                    class: $opts_cl
+                }).appendTo($list);
             }
-            $(this).toggleClass('active').next('ul.options').toggle();
-        });
-        // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
-        // Updates the select element to have the value of the equivalent option
-        $listItems.click(function (e) {
-            e.stopPropagation();
-            $custom_select.text($(this).text()).removeClass('active');
-            $this.val($(this).attr('rel')).change();
-            $list.children('li').removeClass('selected');
-            $(this).addClass('selected');
-            $list.hide();
-            /* alert($this.val()); Uncomment this for demonstration! */
-        });
-        // Hides the unordered list when clicking outside of it
-        $(document).click(function () {
-            $custom_select.removeClass('active');
-            $list.hide();
-        });
+            // Cache the list items
+            let $listItems = $list.children('li');
+            // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
+            $custom_select.click(function (e) {
+                e.stopPropagation();
+                if(!$(this).hasClass('active')){
+                    $('div.custom-select.active').each(function () {
+                        $(this).removeClass('active').next('ul.options').hide();
+                    });
+                }
+                $(this).toggleClass('active').next('ul.options').toggle();
+            });
+            // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
+            // Updates the select element to have the value of the equivalent option
+            $listItems.click(function (e) {
+                e.stopPropagation();
+                $custom_select.text($(this).text()).removeClass('active');
+                $this.val($(this).attr('rel')).change();
+                $list.children('li').removeClass('selected');
+                $(this).addClass('selected');
+                $list.hide();
+                /* alert($this.val()); Uncomment this for demonstration! */
+            });
+            // Hides the unordered list when clicking outside of it
+            $(document).click(function () {
+                $custom_select.removeClass('active');
+                $list.hide();
+            });
 
-    });
-}
+        });
+    }
     
 
     function datepicker(){
-        console.log(1);
         $('.datepicker').datepicker({
             format: 'dd-mm-yyyy',
             autoclose: true,
@@ -146,8 +145,8 @@ function customSelectBox() {
                     '</div>' +
                 '</div>' +
             '</div>' +
-            '<div class="col-two-field container">' +
-                '<div class="bonds-field col-6">' +
+            '<div class="col-two-field">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Bonds Store</label>' +
                     '<select class="bonds-manage-store-select select-hidden"' +
                         'name="bond-store">' +
@@ -156,13 +155,13 @@ function customSelectBox() {
                         '<option value="mmp">Menzies Imports</option>' +
                     '</select>' +
                 '</div>' +
-                '<div class="bonds-field col-6">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Items</label>' +
                     '<input type="text" name="item" required>' +
                 '</div>' +
             '</div>' +
-            '<div class="col-two-field container">' +
-                '<div class="bonds-field col-6">' +
+            '<div class="col-two-field">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Address Book</label>' +
                     '<select class="bonds-address-book-select select-hidden"' +
                         'name="aBook">' +
@@ -172,54 +171,58 @@ function customSelectBox() {
                         '<option value="confirm">Confirm Job Booking</option>' +
                     '</select>' +
                 '</div>' +
-                '<div class="bonds-field col-6">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Weight</label>' +
                     '<input type="text" name="weight" required>' +
                 '</div>' +
             '</div>' +
-            '<div class="col-two-field container">' +
-                '<div class="bonds-field col-6">' +
+            '<div class="col-two-field">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Company</label>' +
                     '<input type="text" name="company" required>' +
                 '</div>' +
-                '<div class="bonds-field col-6">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>No. of Pallets</label>' +
                     '<select class="bonds-pallets-select select-hidden"' +
                         'name="pallets">' +
                         '<option value="1" selected>1</option>' +
                         '<option value="2">2</option>' +
                         '<option value="3">3</option>' +
-                        '<option value="4">4</option>' +
+                        '<option value="4">4</option>' +  
                     '</select>' +
                 '</div>' +
             '</div>' +
-            '<div class="col-two-field container">' +
-                '<div class="bonds-field col-6">' +
+            '<div class="col-two-field">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Street Address</label>' +
                     '<input type="text" name="address" required>' +
                 '</div>' +
-                '<div class="bonds-field col-6">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Cubic Metres</label>' +
                     '<input type="number" name="metres" required>' +
                 '</div>' +
           '</div>' +
-            '<div class="col-two-field container">' +
-                '<div class="bonds-field col-6">' +
+            '<div class="col-two-field">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Suburb / Town</label>' +
                     '<input type="text" name="town" required>' +
                 '</div>' +
-                '<div class="bonds-field col-6">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>References for this leg</label>' +
-                    '<input type="text" name="references" required>' +
-                    '<small>1 lines entered. Max 20 characters per line</small>' +
+                    '<textarea  class="leg-reference-2"' +
+                     'cols="1"' +
+                     'rows="8"' +
+                      'name="leg-reference-2"' +
+                      'required></textarea>' +
+                    '<small><b>0</b> lines entered. Max 20 characters per line</small>' +
                 '</div>' +
             '</div>' +
-            '<div class="col-two-field container">' +
-                '<div class="bonds-field col-6">' +
+            '<div class="col-two-field">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Contact / Phone</label>' +
                     '<input type="text" name="ct-phone" required>' +
                 '</div>' +
-                '<div class="bonds-field col-6">' +
+                '<div class="bonds-field col-12 col-sm-6">' +
                     '<label>Leg Instructions</label>' +
                     '<input type="text" name="instructions" required>' +
                 '</div>' +
